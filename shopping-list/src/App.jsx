a826b67589edeb99ac5568348d9d979b2fd8cc10
Islom <islom.boynazarov@gmail.com>
2026-04-22 23:27:50 +0500

    shopping list react version

---
+++ shopping-list/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+   const [items, setItems] = useState([]);
+   const [inputValue, setInputValue] = useState('');
+   const [filter, setFilter] = useState('');
+
+   function addItem(e) {
+  e.preventDefault();
+
+  if (inputValue === '') {
+    alert('Please add an item');
+    return;
+  }
+
+  setItems([...items, inputValue]);
+  setInputValue('');
+}
+  function removeItem(indexToRemove) {
+  setItems(items.filter((_, index) => index !== indexToRemove));
+}
+  function clearItems() {
+  setItems([]);
+}
+  const filteredItems = items.filter(item =>
+  item.toLowerCase().includes(filter.toLowerCase())
+);
+  return (
+    <div className="container">
+      <header>
+        <h1>Shopping List</h1>
+      </header>
+
+      <form onSubmit={addItem}>
+        <div className="form-control">
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Enter Item"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <button type="submit" className="btn">Add Item</button>
+        </div>
+      </form>
+
+      <div className="filter">
+        <input
+          type="text"
+          className="form-input-filter"
+          placeholder="Filter Items"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+
+      <ul className="items">
+        {filteredItems.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button className="remove-item btn-link text-red" onClick={() => removeItem(index)}>
+              <i className="fa-solid fa-xmark"></i>
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button className="btn-clear" onClick={clearItems}>Clear All</button>
+    </div>
+  );
+}
+
+export default App;
